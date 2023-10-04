3f7ded4dff983ed162cbcbeba8ce2f1300445306
Michael Law <1365977+lawmicha@users.noreply.github.com>
2023-10-04 11:32:07 -0400

    Initial Commit

---
+++ watchOSTestApp Watch App/watchOSTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSTestAppApp.swift
+//  watchOSTestApp Watch App
+//
+//  Created by Law, Michael on 2023-10-04.
+//
+
+import SwiftUI
+
+@main
+struct watchOSTestApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchOSTestApp Watch AppUITests/watchOSTestApp_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  watchOSTestApp_Watch_AppUITestsLaunchTests.swift
+//  watchOSTestApp Watch AppUITests
+//
+//  Created by Law, Michael on 2023-10-04.
+//
+
+import XCTest
+
+final class watchOSTestApp_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
