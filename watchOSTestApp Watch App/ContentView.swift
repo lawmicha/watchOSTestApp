e237511330f3bdb3b5a7471cf757bfabb1c86195
Michael Law <1365977+lawmicha@users.noreply.github.com>
2023-11-10 11:18:13 -0500

    add audio session activate

--- watchOSTestApp Watch App/ContentView.swift
+++ watchOSTestApp Watch App/ContentView.swift
@@ -7,8 +7,49 @@
 
 import SwiftUI
 import Amplify
+import AWSPluginsCore
+import AVFAudio
 
+class SubscriptionViewModel: ObservableObject {
+    
+    var subscription: AmplifyAsyncThrowingSequence<GraphQLSubscriptionEvent<MutationSyncResult>>?
+    
+    func createSubscription() {
+        subscription = Amplify.API.subscribe(request: .subscription(to: User.self, 
+                                                                    subscriptionType: .onCreate))
+        Task {
+            do {
+                guard let subscription = subscription else {
+                    return
+                }
+                for try await subscriptionEvent in subscription {
+                    switch subscriptionEvent {
+                    case .connection(let subscriptionConnectionState):
+                        print("Subscription connect state is \(subscriptionConnectionState)")
+                    case .data(let result):
+                        switch result {
+                        case .success(let createdTodo):
+                            print("Successfully got User from subscription: \(createdTodo)")
+                        case .failure(let error):
+                            print("Got failed result with \(error.errorDescription)")
+                        }
+                    }
+                }
+            } catch {
+                print("Subscription has terminated with \(error)")
+            }
+        }
+    }
+    
+    func cancelSubscription() {
+        // Cancel the subscription listener when you're finished with it
+        subscription?.cancel()
+    }
+}
 struct ContentView: View {
+    
+    @ObservedObject var vm: SubscriptionViewModel = SubscriptionViewModel()
+    
     var body: some View {
         ScrollView {
     
@@ -51,6 +92,28 @@ struct ContentView: View {
                     let tests = try await Amplify.DataStore.query(Test.self)
                     print(tests)
                 }
+            }
+            
+            Button("Create Sub") {
+                Task {
+                    vm.createSubscription()
+                }
+            }
+            
+            Button("Activate Audio") {
+                do {
+                    try AVAudioSession.sharedInstance().setCategory(.playback, 
+                                                                    mode: .default,
+                                                                    policy: .longFormAudio,
+                                                                    options: [])
+                } catch {
+                    print("AVAudioSession setCategory error: \(error)")
+                }
+
+                AVAudioSession.sharedInstance().activate(options: [], completionHandler: { _, _ in 
+                    // The audio route picket may be displayed before the completionHandler is called.
+                    print("AVAudioSession activated.")
+                })
             }
             
         }
